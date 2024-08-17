70c7b7d9135bf520b39fa6572fbae75f7ec66c36
Sakib928 <sakibgscian@gmail.com>
2024-08-17 15:42:55 +0600

    minor changes

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1towayy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -28,11 +27,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-
         const productCollection = client.db('tech-shoi').collection('products');
 
 
@@ -68,10 +62,8 @@ async function run() {
             res.send({ count })
         })
 
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+
     }
 }
 
